b63787a4b4699900616df8f91c249ea8ebc12207
Damien Le Moal <damien.lemoal@wdc.com>
2021-12-03 09:39:46 +0900

    Add search functionality
    Using the docusaurus-search-local plugin.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,11 +16,60 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  /**
-   * Google analytics plugin is already included with preset-classic
-   * so we do not need to add it explicitly.
-   * plugins: ['@docusaurus/plugin-google-analytics'],
-   */
+  plugins: [
+    /**
+     * Google analytics plugin is already included with preset-classic
+     * so we do not need to add it explicitly.
+     * ['@docusaurus/plugin-google-analytics'],
+     */
+    [require.resolve('@cmfcmf/docusaurus-search-local'), {
+      /** whether to index docs pages */
+      indexDocs: true,
+      docsRouteBasePath: '/docs',
+
+      /**
+       * Whether to also index the titles of the parent categories in the
+       * sidebar of a doc page:
+       * 0 disables this feature.
+       * 1 indexes the direct parent category in the sidebar of a doc page
+       * 2 indexes up to two nested parent categories of a doc page
+       * 3...
+       *
+       * Do _not_ use Infinity, the value must be a JSON-serializable integer.
+       */
+      indexDocSidebarParentCategories: 0,
+
+      /** whether to index blog pages */
+      indexBlog: false,
+      /** blogRouteBasePath: '/blog', */
+
+      /** whether to index static pages (/404.html is never indexed) */
+      indexPages: false,
+
+      /** language of your documentation */
+      language: "en",
+
+      /**
+       * Setting this to "none" will prevent the default CSS to be included. The default CSS
+       * comes from autocomplete-theme-classic, which you can read more about here:
+       * https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
+       */
+      style: undefined,
+
+      /** lunr.js-specific settings */
+      lunr: {
+	/**
+	 * When indexing your documents, their content is split into "tokens".
+	 * Text entered into the search box is also tokenized.  This setting
+	 * configures the separator used to determine where to split the text into
+	 * tokens.  By default, it splits the text at whitespace and dashes.
+	 * Note: Does not work for "ja" and "th" languages, since these use a
+	 * different tokenizer.
+	 */
+        tokenizerSeparator: /[\s\-]+/
+      }
+    }]
+  ],
 
   presets: [
     [
